d08150dbe6bf295f9678e88307abb9b346a4aafe
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2011-09-27 21:43:25 -0300

    Add click events.

--- src/game/MemoryGame/ImageAdapter.java
+++ src/game/MemoryGame/ImageAdapter.java
@@ -13,9 +13,15 @@ import java.util.ArrayList;
 
 import android.util.Log;
 
+import java.lang.Thread;
+import java.lang.InterruptedException;
+
+
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
     private Integer[] pieces;
+    private List imageViews;
+    private int piece_up = -1;
 
     public ImageAdapter(Context c) {
         mContext = c;
@@ -26,7 +32,24 @@ public class ImageAdapter extends BaseAdapter {
         }
         Collections.shuffle(ipieces);
         pieces = (Integer[]) ipieces.toArray(new Integer[0]);
+        _createImageViews();
+    }
 
+    private void _createImageViews() {
+        imageViews = new ArrayList();
+        for(int position = 0; position < getCount(); position++) {
+            ImageView imageView;
+
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(60, 80));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(2, 2, 2, 2);
+
+            imageView.setImageResource(R.drawable.back);
+            imageViews.add(imageView);
+
+            installClick(position);
+        }
     }
 
     public int getCount() {
@@ -34,30 +57,64 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     public Object getItem(int position) {
-        return null;
+        return imageViews.get(position);
     }
 
     public long getItemId(int position) {
-        return 0;
+        return pieces[position].longValue();
     }
 
     // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {  // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(60, 80));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(2, 2, 2, 2);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-        int i = pieces[position];
-        Log.d("ImageAdapter", Integer.toString(i));
-        imageView.setImageResource(mThumbIds[i]);
-        return imageView;
+    public synchronized View getView(int position, View convertView, ViewGroup parent) {
+        return (ImageView) imageViews.get(position);
     }
 
+    public void installClick(int position) {
+        // final int pos = position;
+        Log.d("ImageAdapter", "click *" + Integer.toString(position));
+        ImageView imageView =(ImageView)  imageViews.get(position);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int pos = imageViews.indexOf((ImageView) v);
+                Log.d("ImageAdapter", "click!" + Integer.toString(pos));
+                show(pos);
+
+                // FIXME: UI update
+                // http://developer.android.com/resources/articles/timed-ui-updates.html
+                if (piece_up == -1) {
+                    // first click
+                    piece_up = pos;
+                } else {
+                    // second click
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException except) {
+                        // none
+                    }
+                    // hide(position);
+                    // hide(piece_up);
+                    piece_up = -1;
+                }
+
+            }
+        });
+    }
+
+    public void hide(int position) {
+        ImageView img;
+        img = (ImageView) imageViews.get(position);
+        int piece = pieces[position];
+        img.setImageResource(R.drawable.back);
+    }
+
+    public void show(int position) {
+        ImageView img;
+        img = (ImageView) imageViews.get(position);
+        int piece = pieces[position];
+        img.setImageResource(mThumbIds[piece]);
+
+    }
+    //
     // references to our images
     private Integer[] mThumbIds = {
             R.drawable.american_gangster_icon,
